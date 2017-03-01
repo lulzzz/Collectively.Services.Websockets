e3262296ca2d01590db12e2dab51f0f967d7eda7
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-03-01 13:32:20 +0100

    lockbox client added

--- src/index.js
+++ src/index.js
@@ -1,77 +1,19 @@
-var express = require('express');
-var http = require('http');
-var socketio = require('socket.io');
-var amqp = require('amqp');
 var config = require('config');
-var RabbitMqConnection = require('./lib/RabbitMqConnection.js');
-var OperationMessageHandler = require('./lib/OperationMessageHandler.js');
-var RemarkMessageHandler = require('./lib/RemarkMessageHandler.js');
+var WebSocketService = require('./WebsocketService.js');
 
-console.log('Starting Collectively Websockets Service...');
+console.log('Fetching configuration data');
 var env = process.env.NODE_ENV || 'local';
 if(env === 'production' || env === 'development') {
-  //TODO lockbox 
+  let lockboxClient = require('./lib/LockBoxClient.js');
+  lockboxClient.getConfig()
+    .then((configuration) => {
+      console.log('Configuration fetched from LockBox');
+      let service = new WebSocketService(configuration);
+      service.run();
+    })
 } else {
   var configuration = config.get('config');
+  let service = new WebSocketService(configuration);
+  service.run();
 }
 
-var port = configuration.port || 15000;
-var rabbitMqConfig = configuration.rabbitMq;
-var app = express();
-var server = http.createServer(app);
-var io = socketio(server);
-
-var operationMessageHandler = new OperationMessageHandler(io);
-var remarkMessageHandler = new RemarkMessageHandler(io);
-var rmqConnection = new RabbitMqConnection(rabbitMqConfig);
-
-rmqConnection.subscribe('coolector.services.operations.shared.events',
-  'operationupdated.#',
-  operationMessageHandler.publishOperationUpdated);
-  
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'remarkcreated.#',
-  remarkMessageHandler.publishRemarkCreated);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'remarkresolved.#',
-  remarkMessageHandler.publishRemarkResolved);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'remarkdeleted.#',
-  remarkMessageHandler.publishRemarkDeleted);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'photostoremarkadded.#',
-  remarkMessageHandler.publishPhotosToRemarkAdded);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'photosfromremarkremoved.#',
-  remarkMessageHandler.publishPhotosFromRemarkRemoved);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'remarkvotesubmitted.#',
-  remarkMessageHandler.publishRemarkVoteSubmitted);
-
-rmqConnection.subscribe('coolector.services.remarks.shared.events',
-  'remarkvotedeleted.#',
-  remarkMessageHandler.publishRemarkVoteDeleted);
-
-rmqConnection.connect();
-
-function onSocketConnection(socket) {
-  console.log('connected');
-  socket.on('initialize', (data) => {
-    console.log('initialized');
-  });
-}
-
-io.on('connection', onSocketConnection);
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
-
-app.get('/', (req, res) => {
-  res.send('Collectively Websocket Service');
-});
