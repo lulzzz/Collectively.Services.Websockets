7048b5cabc79724c47bbc9fbeb6ac86d5c2aee65
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-03-13 10:29:04 +0100

    cors enabled

--- src/lib/WebsocketService.js
+++ src/lib/WebsocketService.js
@@ -63,7 +63,20 @@ module.exports = function(configuration) {
       });
     }
 
-    io.on('connection', onSocketConnection);
+    io.set('origins', '*:*');
+
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+      res.header('Access-Control-Expose-Headers', 'Content-Length');
+      res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+      if (req.method === 'OPTIONS') {
+        return res.send(200);
+      } else {
+        return next();
+      }
+    });
 
     server.listen(port, () => {
       console.log('Server listening at port %d', port);
@@ -72,5 +85,7 @@ module.exports = function(configuration) {
     app.get('/', (req, res) => {
       res.send('Collectively Websocket Service');
     });
+
+    io.on('connection', onSocketConnection);    
   }
 };
